233d3c30e7e89b0b515cc7088c403b15fd5c3157
juriasr <juriasr@gmail.com>
2022-05-18 22:28:45 -0600

    Initial Commit

---
+++ users/usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  usersApp.swift
+//  users
+//
+//  Created by jose Urias on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
